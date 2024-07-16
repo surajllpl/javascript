62049cdcaa87dad2a94e63055f82b110bf3295b6
surajllpl <srj.llpl@gmail.com>
2024-07-16 17:03:41 +0530

    basics of javascript

---
+++ front_end/22 Intro to arrays.js
@@ -0,0 +1,10 @@
+// Array
+// refrance type 
+//order collection of elements
+let fruits = ["apple","orange","mango"];
+let numbers = [1,2,3,4,5];
+let mix = [1,2,3,"string",null,true];
+
+console.log(fruits[1]);
+console.log(numbers);
+console.log(mix);
