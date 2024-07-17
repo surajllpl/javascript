ad21418205af03285e5401f97d0b55fa4a05ebd9
surajllpl <srj.llpl@gmail.com>
2024-07-17 13:19:43 +0530

    Array in javascript

--- front_end/22 Intro to arrays.js
+++ front_end/22 Intro to arrays.js
@@ -8,3 +8,12 @@ let mix = [1,2,3,"string",null,true];
 console.log(fruits[1]);
 console.log(numbers);
 console.log(mix);
+
+console.log(fruits);
+
+fruits[1] = "banana";
+let obj ={}; // object literal
+console.log(fruits,typeof(fruits)); // object 
+// how to know fruits object is array or not
+console.log(Array.isArray(fruits)); // true
+console.log(Array.isArray(obj)); // false
