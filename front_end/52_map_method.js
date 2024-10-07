944c6d9b209b4e2c8f175f9e5c5b92ec040904d6
surajllpl <srj.llpl@gmail.com>
2024-10-07 11:20:23 +0530

    Important Array Methods update

--- front_end/52_map_method.js
+++ front_end/52_map_method.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // map method
 
 const numbers =[3,4,5,1,6];
@@ -26,32 +25,3 @@ const username = users.map((user)=>{
     return user.firstname;
 });
 console.log(username);
-=======
-// map method
-
-const numbers =[3,4,5,1,6];
-
-// const square = (number)=>{
-//     return number * number;
-// } 
-
-const squareNo = numbers.map((number)=>{
-    return number * number;
-});
-
-console.log(squareNo);
-
-
-const users =[
-    {firstname : "Tom",age:25},
-    {firstname : "John",age:30},
-    {firstname : "Anna",age:35}
-    
-];
-
-
-const username = users.map((user)=>{
-    return user.firstname;
-});
-console.log(username);
->>>>>>> 501c903e6ec509644dafbf47244d06c26c57b065
