121014950d56498c924a4531c7839f453ca27198
surajllpl <152859410+surajllpl@users.noreply.github.com>
2024-10-24 22:36:36 +0530

    Closures Example 2

---
+++ part_2/06_closures/06_closures_2.js
@@ -0,0 +1,13 @@
+// Closures examplee 2
+
+function hello(x){
+    const a = "varA";
+    const b = "varB";
+    return function(){
+        console.log(a,b,x);
+    }
+
+}
+
+const ans = hello("arg");
+ans();
