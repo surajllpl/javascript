3b99805c244e00f568c5751f0ed70319fb99e5d5
surajllpl <srj.llpl@gmail.com>
2024-10-25 12:22:57 +0530

    Closures Example 3

---
+++ part_2/06_closures/06_closures_3.js
@@ -0,0 +1,25 @@
+// Closures Example 3
+
+
+// function myFunction(power){
+//     return function(number){
+//         return number ** power;
+//     }
+// }
+
+// const square = myFunction(2);
+// const ans = square(3);
+// console.log(ans);
+
+
+
+const myFunction =(power)=>{
+    return (number)=>{
+        return number ** power;
+    }
+}
+// const myFunction =power=> number=>number ** power; // 
+
+const square = myFunction(2);
+const ans = square(3);
+console.log(ans);
