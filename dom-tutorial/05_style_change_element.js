f8b88db63ae703d8a4a4622a0d65fd9353638b0e
surajllpl <srj.llpl@gmail.com>
2024-11-25 11:39:30 +0530

    changes in committed section

--- dom-tutorial/05_style_change_element.js
+++ dom-tutorial/05_style_change_element.js
@@ -1,9 +1,7 @@
 // change the style of elements
 
 const mainHeading  = document.querySelector("div.headline h2");
-// console.log(mainHeading.style); // set syle to the selected element
+// console.log(mainHeading.style); // set style to the selected element
 mainHeading.style.color = "red"; // change the color of the text to red
 mainHeading.style.backgroundColor = "blue";
 mainHeading.style.border = "5px solid green";
-
-
